27ec9aa0d452ce917e49aea19815c715f76f49db
Minchan Kim <minchan@kernel.org>
2015-09-08 15:00:24 -0700

    BACKPORT: mm: /proc/pid/smaps:: show proportional swap share of the mapping
    We want to know per-process workingset size for smart memory management
    on userland and we use swap(ex, zram) heavily to maximize memory
    efficiency so workingset includes swap as well as RSS.
    
    On such system, if there are lots of shared anonymous pages, it's really
    hard to figure out exactly how many each process consumes memory(ie, rss
    + wap) if the system has lots of shared anonymous memory(e.g, android).
    
    This patch introduces SwapPss field on /proc/<pid>/smaps so we can get
    more exact workingset size per process.
    
    Bongkyu tested it. Result is below.
    
    1. 50M used swap
    SwapTotal: 461976 kB
    SwapFree: 411192 kB
    
    $ adb shell cat /proc/*/smaps | grep "SwapPss:" | awk '{sum += $2} END {print sum}';
    48236
    $ adb shell cat /proc/*/smaps | grep "Swap:" | awk '{sum += $2} END {print sum}';
    141184
    
    2. 240M used swap
    SwapTotal: 461976 kB
    SwapFree: 216808 kB
    
    $ adb shell cat /proc/*/smaps | grep "SwapPss:" | awk '{sum += $2} END {print sum}';
    230315
    $ adb shell cat /proc/*/smaps | grep "Swap:" | awk '{sum += $2} END {print sum}';
    1387744
    
    [akpm@linux-foundation.org: simplify kunmap_atomic() call]
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Reported-by: Bongkyu Kim <bongkyu.kim@lge.com>
    Tested-by: Bongkyu Kim <bongkyu.kim@lge.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Sergey Senozhatsky <sergey.senozhatsky.work@gmail.com>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Jerome Marchand <jmarchan@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Bug: 26190646
    Change-Id: Idf92d682fdef432bdd66e530a7e7cdff8f375db1
    Signed-off-by: Thierry Strudel <tstrudel@google.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -507,6 +507,7 @@ struct mem_size_stats {
 #ifdef CONFIG_ZNDSWAP
 	u64 pswap_zndswap;
 #endif
+	u64 swap_pss;
 };
 
 #ifdef CONFIG_SWAP
@@ -563,11 +564,22 @@ static void smaps_pte_entry(pte_t *pte, unsigned long addr,
 
 		/* M for pswap interface */
 		if (!non_swap_entry(swpent)) {
+			int mapcount;
 #ifdef CONFIG_SWAP
 			swp_entry_t entry;
 			struct swap_info_struct *p;
 #endif /* CONFIG_SWAP*/
+
 			mss->swap += PAGE_SIZE;
+			mapcount = swp_swapcount(swpent);
+			if (mapcount >= 2) {
+				u64 pss_delta = (u64)PAGE_SIZE << PSS_SHIFT;
+
+				do_div(pss_delta, mapcount);
+				mss->swap_pss += pss_delta;
+			} else {
+				mss->swap_pss += (u64)PAGE_SIZE << PSS_SHIFT;
+			}
 #ifdef CONFIG_SWAP
 			entry = pte_to_swp_entry(*pte);
 			if (non_swap_entry(entry))
@@ -755,6 +767,7 @@ static int show_smap(struct seq_file *m, void *v, int is_pid)
 #ifdef CONFIG_ZNDSWAP
 		   "PSwap_zndswap:  %8lu kB\n"
 #endif
+		   "SwapPss:        %8lu kB\n"
 		   "KernelPageSize: %8lu kB\n"
 		   "MMUPageSize:    %8lu kB\n"
 		   "Locked:         %8lu kB\n",
@@ -775,6 +788,7 @@ static int show_smap(struct seq_file *m, void *v, int is_pid)
 #ifdef CONFIG_ZNDSWAP
 		   (unsigned long)(mss.pswap_zndswap >> (10 + PSS_SHIFT)),
 #endif
+		   (unsigned long)(mss.swap_pss >> (10 + PSS_SHIFT)),
 		   vma_kernel_pagesize(vma) >> 10,
 		   vma_mmu_pagesize(vma) >> 10,
 		   (vma->vm_flags & VM_LOCKED) ?
